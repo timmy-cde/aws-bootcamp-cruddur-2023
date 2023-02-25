4716c41a6322d102663e7159cc94abaf07b467bf
Timothy Manuel <71366703+timmy-cde@users.noreply.github.com>
2023-02-25 22:27:47 +0800

    Update backend-run.sh

--- backend-flask/backend-run.sh
+++ backend-flask/backend-run.sh
@@ -2,9 +2,6 @@
 
 echo 'installing dependencies...'
 pip3 install -r requirements.txt
-apt-get update 
-apt-get install -y gcc
-apt-get install -y curl
 
 echo 'building backend...'
 python3 -m flask run --host=0.0.0.0 --port=4567
