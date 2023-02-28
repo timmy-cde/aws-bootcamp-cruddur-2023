21732506634a3e948da251c9a67d0c20ca0de692
timmy-cde <timothys.manuel@gmail.com>
2023-02-28 15:18:01 +0000

    added cloudwatch, disabled xray and cloudwatch

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -4,7 +4,7 @@ from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
     # xray ----------------
-    subsegment = xray_recorder.begin_subsegment('user_activities')
+    # subsegment = xray_recorder.begin_subsegment('user_activities')
 
     model = {
       'errors': None,
@@ -13,7 +13,7 @@ class UserActivities:
     now = datetime.now(timezone.utc).astimezone()
 
     # xray ----------------
-    subsegment.put_annotation('now', now.isoformat())
+    # subsegment.put_annotation('now', now.isoformat())
 
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
@@ -21,8 +21,8 @@ class UserActivities:
       now = datetime.now()
       
       # xray ----------------
-      subsegment2 = xray_recorder.begin_subsegment('mock-data')
-      subsegment2.put_annotation('now', now.isoformat())
+      # subsegment2 = xray_recorder.begin_subsegment('mock-data')
+      # subsegment2.put_annotation('now', now.isoformat())
 
       results = [{
         'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
@@ -34,10 +34,10 @@ class UserActivities:
       model['data'] = results
 
       # xray ----------------
-      xray_recorder.end_subsegment()
+      # xray_recorder.end_subsegment()
 
     # xray ----------------
-    subsegment.put_annotation('results_length', len(model['data']))
-    xray_recorder.end_subsegment()
+    # subsegment.put_annotation('results_length', len(model['data']))
+    # xray_recorder.end_subsegment()
 
     return model
