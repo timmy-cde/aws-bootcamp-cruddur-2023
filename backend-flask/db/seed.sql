968d46f20aa82a0df9eb4b0fe7d18541cb374e71
timmy-cde <timothys.manuel@gmail.com>
2023-03-24 12:00:46 +0000

    debug seed

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,8 +1,12 @@
 -- this file was manually created
+-- INSERT INTO public.users (display_name, email, handle, cognito_user_id)
+-- VALUES
+--   ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
+  -- ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
+  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'13d4e9ca-6607-4a9c-80e6-cc8a8ad54f7f'),
+  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'9e605c5a-e8b0-4431-a774-686338918171');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
