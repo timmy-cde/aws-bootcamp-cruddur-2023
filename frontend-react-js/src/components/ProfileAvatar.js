2a79bd4234228fba17b6d3aac8f90f9cd84a39dd
timmy-cde <timothys.manuel@gmail.com>
2023-04-21 14:32:03 +0000

    bypass undefined.jpg error

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,6 +1,8 @@
 import "./ProfileAvatar.css";
 
 export default function ProfileAvatar(props) {
+  if (!props.id) return
+  
   const backgroundImage = `url("https://assets.tmanuel.cloud/avatars/${props.id}.jpg")`;
   const styles = {
     backgroundImage: backgroundImage,
