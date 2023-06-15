2add1b9363d24de9b554122dbfc3fe99abe5701d
timmy-cde <timothys.manuel@gmail.com>
2023-06-15 08:38:47 +0000

    refactor the app.py

---
+++ backend-flask/lib/cloudwatch.py
@@ -0,0 +1,18 @@
+import watchtower
+import logging
+from time import strftime
+from flask import request
+
+# Configuring Logger to Use CloudWatch
+# LOGGER = logging.getLogger(__name__)
+# LOGGER.setLevel(logging.DEBUG)
+# console_handler = logging.StreamHandler()
+# cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+# LOGGER.addHandler(console_handler)
+# LOGGER.addHandler(cw_handler)
+# LOGGER.info("Test Log")
+
+def init_cloudwatch(response):
+    timestamp = strftime('[%Y-%b-%d %H:%M]')
+    LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+    return response
--- frontend-react-js/src/pages/NotificationsFeedPage.js
+++ frontend-react-js/src/pages/NotificationsFeedPage.js
@@ -1,7 +1,7 @@
 import "./NotificationsFeedPage.css";
 import React from "react";
 
-import { Auth } from "aws-amplify";
+import { checkAuth, getAccessToken } from "lib/CheckAuth";
 
 import DesktopNavigation from "../components/DesktopNavigation";
 import DesktopSidebar from "../components/DesktopSidebar";
@@ -20,7 +20,12 @@ export default function NotificationsFeedPage() {
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/notifications`;
+      await getAccessToken();
+      const access_token = localStorage.getItem("access_token");
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+        },
         method: "GET",
       });
       let resJson = await res.json();
@@ -34,33 +39,13 @@ export default function NotificationsFeedPage() {
     }
   };
 
-  const checkAuth = async () => {
-    Auth.currentAuthenticatedUser({
-      // Optional, By default is false.
-      // If set to true, this call will send a
-      // request to Cognito to get the latest user data
-      bypassCache: false,
-    })
-      .then((user) => {
-        console.log("user", user);
-        return Auth.currentAuthenticatedUser();
-      })
-      .then((cognito_user) => {
-        setUser({
-          display_name: cognito_user.attributes.name,
-          handle: cognito_user.attributes.preferred_username,
-        });
-      })
-      .catch((err) => console.log(err));
-  };
-
   React.useEffect(() => {
     //prevents double call
     if (dataFetchedRef.current) return;
     dataFetchedRef.current = true;
 
     loadData();
-    checkAuth();
+    checkAuth(setUser);
   }, []);
 
   return (
