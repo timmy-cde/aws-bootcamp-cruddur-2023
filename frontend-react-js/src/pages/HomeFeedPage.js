746560d76bc7a3ef9f041d08706ce3c77478a789
timmy-cde <timothys.manuel@gmail.com>
2023-06-25 14:45:39 +0000

    update task def and homefeed

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -54,7 +54,7 @@ export default function HomeFeedPage() {
         />
         <div className="activity_feed">
           <div className="activity_feed_heading">
-            <div className="title">Home😎</div>
+            <div className="title">Home</div>
           </div>
           <ActivityFeed
             title="Home"
